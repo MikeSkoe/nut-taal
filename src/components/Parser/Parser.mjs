9d4e904d89a6884f7631fbe3b648b952b8470e87
Mike Skoe <rapidmajk@yandex.ru>
2023-06-15 23:35:16 +0400

    Correctly split text with question sentences

--- src/components/Parser/Parser.mjs
+++ src/components/Parser/Parser.mjs
@@ -4,8 +4,11 @@ import * as Lang from "../../Lang.mjs";
 import * as Curry from "rescript/lib/es6/curry.js";
 import * as Utils from "../../Utils.mjs";
 import * as React from "react";
-import * as $$String from "rescript/lib/es6/string.js";
 import * as Belt_List from "rescript/lib/es6/belt_List.js";
+import * as Belt_Array from "rescript/lib/es6/belt_Array.js";
+import * as Belt_Option from "rescript/lib/es6/belt_Option.js";
+
+var sentenceSplitterRe = /\s*[.!?]\s*/;
 
 function Parser(props) {
   var conjunctions = props.conjunctions;
@@ -18,7 +21,14 @@ function Parser(props) {
       });
   var setParsed = match[1];
   React.useEffect((function () {
-          var res = Belt_List.map($$String.split_on_char(/* '.' */46, text), (function (line) {
+          var res = Belt_List.map(Belt_Array.reduce(text.split(sentenceSplitterRe), /* [] */0, (function (acc, curr) {
+                      return Belt_Option.mapWithDefault(curr, acc, (function (curr) {
+                                    return Belt_List.concat(acc, {
+                                                hd: curr,
+                                                tl: /* [] */0
+                                              });
+                                  }));
+                    })), (function (line) {
                   return Curry._2(Lang.Lang.parse, conjunctions, line);
                 }));
           Curry._1(setParsed, (function (param) {
@@ -33,7 +43,7 @@ function Parser(props) {
             }, Belt_List.toArray(Belt_List.flatten(Utils.putBetween(Belt_List.map(match[0], (function (line) {
                                 return Utils.putBetween(Curry._1(Lang.Lang.show, line), " ");
                               })), {
-                          hd: ". ",
+                          hd: React.createElement("br", undefined),
                           tl: /* [] */0
                         }))));
 }
@@ -41,6 +51,7 @@ function Parser(props) {
 var make = Parser;
 
 export {
+  sentenceSplitterRe ,
   make ,
 }
 /* Lang Not a pure module */
--- src/components/Parser/Parser.res
+++ src/components/Parser/Parser.res
@@ -1,13 +1,18 @@
 open Lang
 open Belt
 
+let sentenceSplitterRe = %re("/\s*[.!?]\s*/");
+
 @react.component
 let make = (~text: string, ~conjunctions) => {
     let (parsed, setParsed) = React.useState(_ => list{Lang.empty});
 
     React.useEffect2(() => {
         text
-        -> String.split_on_char('.', _)
+        -> Js.String2.splitByRe(sentenceSplitterRe) 
+        -> Array.reduce(list{}, (acc, curr) =>
+            curr -> Option.mapWithDefault(acc, curr => List.concat(acc, list{curr}))
+        )
         -> List.map(line => Lang.parse(conjunctions, line))
         -> res => setParsed(_ => res)
         -> _ => None;
@@ -16,7 +21,7 @@ let make = (~text: string, ~conjunctions) => {
     <div className="parsed">{
         parsed
         -> List.map(line => line -> Lang.show -> Utils.putBetween(React.string(" ")))
-        -> Utils.putBetween(list{". " -> React.string})
+        -> Utils.putBetween(list{<br />})
         -> List.flatten
         -> List.toArray
         -> React.array
